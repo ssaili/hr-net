add3cd920813896f2f0976c4039d07fa9f024d5c
ssaili <ssaili.fr@gmail.com>
2024-02-21 23:49:10 +0100

    Refactor form component and remove unused code

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,22 +1,92 @@
+import { useState } from "react";
 import "./style.scss";
 import Input from "../Input";
-import SelectInput from "../SelectInput";
 import departments from "../../data/departments";
 import states from "../../data/states";
-import DateInput from "../DateInput";
 
 function Form() {
+  const [inputValues, setInputValues] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    startDate: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    department: "",
+  });
+
+  const handleInputChange = (name, newValue) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [name]: newValue,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValues);
+  };
+
   return (
-    <form className="form">
-      <Input label="First Name" type="text" />
-      <Input label="Last Name" type="text" />
-      <DateInput label="Date of Birth" />
-      <DateInput label="Start Date" />
-      <Input label="Street" type="text" />
-      <Input label="City" type="text" />
-      <SelectInput label="State" options={states} />
-      <Input label="Zip Code" type="number" />
-      <SelectInput label="Department" options={departments} />
+    <form className="form" onSubmit={handleSubmit}>
+      <Input
+        label="First Name"
+        type="text"
+        name="firstName"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Last Name"
+        type="text"
+        name="lastName"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Date of Birth"
+        type="date"
+        name="dateOfBirth"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Start Date"
+        type="date"
+        name="startDate"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Street"
+        type="text"
+        name="street"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="City"
+        type="text"
+        name="city"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="State"
+        type="select"
+        name="state"
+        options={states}
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Zip Code"
+        type="number"
+        name="zipCode"
+        onInputChange={handleInputChange}
+      />
+      <Input
+        label="Department"
+        type="select"
+        name="department"
+        options={departments}
+        onInputChange={handleInputChange}
+      />
       <button type="submit" className="form-submit">
         Save
       </button>
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,17 +1,97 @@
+import { useState } from "react";
+//DatePicker library is imported from react-datepicker
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+//Select library is imported from react-select
+import Select from "react-select";
 import "./style.scss";
 
-function Input({ label, type, value, onChange }) {
-  return (
-    <div className="input">
-      <label className="input-label">{label}</label>
-      <input
-        className="input-field"
-        type={type}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
+function Input({ label, type, name, options, onInputChange }) {
+  const [inputValue, setInputValue] = useState(getInitialValue(type));
+
+  const handleChange = (event) => {
+    const updateInputValue = (value) => {
+      setInputValue(value);
+      onInputChange(name, value);
+    };
+
+    switch (type) {
+      case "date":
+        if (event) {
+          const formattedDate = event.toLocaleDateString(
+            document.documentElement.lang
+          );
+          updateInputValue(formattedDate);
+        }
+        break;
+      case "select":
+        if (event) {
+          const selectedValue = event.value;
+          updateInputValue(selectedValue);
+        }
+        break;
+      default:
+        if (event && event.target) {
+          const inputValue = event.target.value;
+          updateInputValue(inputValue);
+        }
+        break;
+    }
+  };
+
+  function getInitialValue(type) {
+    switch (type) {
+      case "date":
+        return new Date();
+      case "select":
+        return null;
+      default:
+        return "";
+    }
+  }
+
+  function renderInput() {
+    switch (type) {
+      case "date":
+        return (
+          <div className="date-container">
+            <label className="date-label">{label}</label>
+            <DatePicker
+              className="date-field"
+              selected={inputValue}
+              onChange={handleChange}
+            />
+          </div>
+        );
+      case "select":
+        return (
+          <div className="select-container">
+            <label className="select-label">{label}</label>
+            <Select
+              className="select-field"
+              classNamePrefix="select"
+              defaultValue={inputValue}
+              onChange={handleChange}
+              options={options}
+            />
+          </div>
+        );
+      default:
+        return (
+          <div className="input-container">
+            <label className="input-label">{label}</label>
+            <input
+              className="input-field"
+              type={type}
+              value={inputValue}
+              onChange={handleChange}
+            />
+          </div>
+        );
+    }
+  }
+
+  return renderInput();
 }
 
 export default Input;
