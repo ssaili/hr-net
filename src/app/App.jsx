8bc02230985212e8c564766fb18632431c5268d4
ssaili <ssaili.fr@gmail.com>
2024-02-28 11:42:12 +0100

    Add @reduxjs/toolkit and react-redux dependencies

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CurrentEmployeesPage from "./pages/CurrentEmployeesPage";
-import ErrorPage from "./pages/ErrorPage";
+import HomePage from "../pages/HomePage";
+import CurrentEmployeesPage from "../pages/CurrentEmployeesPage";
+import ErrorPage from "../pages/ErrorPage";
 
 function App() {
   const router = createBrowserRouter([
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app/App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
